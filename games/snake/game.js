acdcee27071ff839195185392a037fe3dbd0abf4
ofek shtrit <ofek1997shtrit@gmail.com>
2022-06-13 00:25:22 +0300

    swipe on mobile

--- games/snake/game.js
+++ games/snake/game.js
@@ -5,6 +5,11 @@ import { outsideGird } from './grid.js'
 let flag = true;
 let lastRenderTime = 0;
 let gameOver = false
+let startX;
+let startY;
+let endX;
+let endY;
+
 const gameBoard = document.getElementById('game-board')
 
 function mainGame(currentTime) {
@@ -43,3 +48,52 @@ function render() {
 function checkDeath() {
   gameOver = outsideGird(snakeHead) || SnakeAteItSelf()
 }
+
+// function mobileTesting() {
+//   window.addEventListener('touchmove', (e) => {
+//     const touchList = e.changedTouches;
+//     confirm(`${touchList.item(0).radiusX}`) })
+// }
+
+function mobileTesting(params) {
+  window.addEventListener('touchstart', (e) => {
+    const touchList = e.changedTouches;
+    const swipe = touchList.item(0)
+    startX = swipe.screenX
+    startY = swipe.screenY
+  })
+  window.addEventListener('touchend', (e) => {
+    const touchList = e.changedTouches;
+    const swipe = touchList.item(0)
+    endX = swipe.screenX
+    endY = swipe.screenY
+    desideSwipes()
+  })
+}
+function desideSwipes() {
+  // confirm(`startX: ${startX}, endX: ${endX}, startY: ${startY}, endY: ${endY}`)
+  const distanceHorizontal = Math.abs(startX - endX)
+  const distanceVertical = Math.abs(startY - endY)
+  if (distanceHorizontal > distanceVertical) {
+    // thre user attempted horizontal swipe
+    if (startX < endX) {
+      console.log('swipe right!')
+    } else {
+      console.log('swipe left!')
+    }
+  } else if (distanceHorizontal < distanceVertical) {
+    // the user attempted vertical swipe
+    if (startY < endY) {
+      console.log('swipe down!')
+    } else {
+      console.log('swipe up!')
+    }
+  }
+  // if (startX > endX) {
+  //   console.log('swipe left')
+  // } else {
+  //   console.log('swipt right')
+  // }
+}
+
+document.addEventListener("DOMContentLoaded", mobileTesting);
